d179f1fa0c40bfcd371677f05ed9a08b0d7019d5
Isaac Kargar <eshagh.kargar@aalto.fi>
2021-05-20 11:52:39 +0300

    add env settings to main example file

--- examples/fluids_gym_example.py
+++ examples/fluids_gym_example.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import fluids
 from gym_fluids.envs.fluids_env import FluidsEnv, FluidsVelEnv
+from fluids.assets import * 
+import cv2
+
 
 obs_w = 500
-time_limit = 100
+time_limit = 1000
 
 fluids_args = {
     "visualization_level": 4,
-    "fps": 30,
+    "fps": 10,
     "obs_args": {"obs_dim": obs_w},
     "obs_space": fluids.OBS_BIRDSEYE,
     "background_control": fluids.BACKGROUND_CSP,
@@ -34,9 +37,26 @@ env.reset()
 
 action = [0, 0]  # [steer, acc]
 reward = 0
+t = 0
 while True:
     obs, rew, done, info = env.step(action)
+    obs = np.transpose(obs, (1, 0, 2))
+    if t % 10 == 0:
+        resized = cv2.resize(obs, (64, 64), interpolation = cv2.INTER_AREA)
+        plt.imshow(resized)
+        plt.show()
+
     reward += rew
     env.render()
-    print(rew, action)
+    # print(rew, action)
     action = gym_fluids.agents.fluids_supervisor(obs, info)
+    t += 1
+    if done: 
+        break
+
+"""
+to get dynamic objects' info use:
+dynamic objects are CrossWalkLight, CrossWalkLight, Pedestrian.   
+check if type(env.fluidsim.state.dynamic_objects[113]) == Car
+then get values you want using env.fluidsim.state.dynamic_objects[id].x or .y or .vel 
+"""
